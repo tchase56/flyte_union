de3430884dbeee9fd548506cd015fe80cbc18a92
Jan Fiedler <jan@union.ai>
2024-10-04 17:27:49 -0700

    add wandb logging union metadata

--- wine_classification_hyperparameter_search_v3.py
+++ wine_classification_hyperparameter_search_v3.py
@@ -3,9 +3,14 @@ from itertools import product
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_wine
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score, confusion_matrix, classification_report, accuracy_score
+from sklearn.metrics import (
+    f1_score,
+    confusion_matrix,
+    classification_report,
+    accuracy_score,
+)
 from flytekit import task, workflow, Secret, current_context
 from flytekit import task
 
@@ -29,19 +34,20 @@ import wandb
 # )
 
 sklearn_image_spec = ImageSpec(
-    name='wine',
-    requirements='requirements_prod.txt',
-    registry='us-east4-docker.pkg.dev/union-ai-poc/fbin-union-ai-poc-docker',
-    base_image="ghcr.io/flyteorg/flytekit:py3.11-latest"
+    name="wine",
+    requirements="requirements_prod.txt",
+    registry="us-east4-docker.pkg.dev/union-ai-poc/fbin-union-ai-poc-docker",
+    base_image="ghcr.io/flyteorg/flytekit:py3.11-latest",
 )
 
 cache_version = "cache-v1"
 cache = False
 
 wandb_project_name = "wine-classification-test"
-wandb_secret = Secret(key='WANDB_API_KEY')
-wandb_project = 'eif'
-wandb_entity = 'tyler-chase-fortune-brands-innovations'
+wandb_secret = Secret(key="WANDB_API_KEY")
+wandb_project = "eif"
+wandb_entity = "tyler-chase-fortune-brands-innovations"
+
 
 # @task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version)
 @task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version)
@@ -49,16 +55,21 @@ wandb_entity = 'tyler-chase-fortune-brands-innovations'
 # @task()
 # @task(container_image=sklearn_image_spec)
 def get_data() -> pd.DataFrame:
-    # Load Data 
+    # Load Data
     data = load_wine(as_frame=True).frame
 
     return data
 
+
 @task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version)
-def split_data(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def split_data(
+    data: pd.DataFrame,
+) -> Tuple[
+    pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame
+]:
     """
     Split the data into training and testing sets using stratified sampling
-    
+
     Parameters:
         data (pd.DataFrame): The wine dataset
 
@@ -71,21 +82,31 @@ def split_data(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataF
     """
 
     # Split Data
-    X_data = data.drop(columns = ["target"])
+    X_data = data.drop(columns=["target"])
     y_data = data[["target"]]
-    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.25, stratify=y_data, random_state=42)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, stratify=y_train, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_data, y_data, test_size=0.25, stratify=y_data, random_state=42
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.25, stratify=y_train, random_state=42
+    )
 
     return X_train, X_val, X_test, y_train, y_val, y_test
-    
-@task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version, secret_requests = [wandb_secret])
-@wandb_init(project = wandb_project, entity = wandb_entity, secret=wandb_secret)
+
+
+@task(
+    container_image=sklearn_image_spec,
+    cache=cache,
+    cache_version=cache_version,
+    secret_requests=[wandb_secret],
+)
+@wandb_init(project=wandb_project, entity=wandb_entity, secret=wandb_secret)
 def train_model(
     X_train: pd.DataFrame,
     y_train: pd.DataFrame,
     X_val: pd.DataFrame,
     y_val: pd.DataFrame,
-    hyperparameters: Dict[str, Optional[float|str|int|None]],
+    hyperparameters: Dict[str, Optional[float | str | int | None]],
 ) -> RandomForestClassifier:
     """
     Trains a random forest classifier model using the given dataframe and hyperparameters
@@ -102,18 +123,27 @@ def train_model(
     """
 
     model = RandomForestClassifier(**hyperparameters)
-    model.fit(X_train, y_train['target'])
+    model.fit(X_train, y_train["target"])
     y_pred = model.predict(X_val)
-    for i in hyperparameters:
-        wandb.log({i: hyperparameters[i]})
-    wandb.log({"accuracy": accuracy_score(y_val, y_pred)})
+
+    union_metadata = {
+        "UNION_WORKFLOW_NAME": training_workflow.name,
+        "UNION_EXECUTION_ID": current_context().execution_id.name,
+    }
+    model_data = {"accuracy": accuracy_score(y_val, y_pred)}
+
+    log_params = hyperparameters | union_metadata | model_data
+
+    for i in log_params:
+        wandb.log({i: log_params[i]})
 
     return model
 
+
 @task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version)
 def create_search_grid(
-    grid: Dict[str, List[Optional[float|str|int|None]]]
-) -> List[Dict[str, Optional[float|str|int|None]]]:
+    grid: Dict[str, List[Optional[float | str | int | None]]]
+) -> List[Dict[str, Optional[float | str | int | None]]]:
     """
     Generate a search grid based on the given dictionary of lists.
 
@@ -123,19 +153,26 @@ def create_search_grid(
     Returns:
         list[dict[str, Optional[float|str|int|None]]]: Each possible permutation of the hyperparameter choices.
     """
-    
-    
-    product_values = product(*[v if isinstance(v, (list, tuple)) else [v] for v in grid.values()])
-    
+
+    product_values = product(
+        *[v if isinstance(v, (list, tuple)) else [v] for v in grid.values()]
+    )
+
     return [dict(zip(grid.keys(), values)) for values in product_values]
 
-@task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version, enable_deck=True)
+
+@task(
+    container_image=sklearn_image_spec,
+    cache=cache,
+    cache_version=cache_version,
+    enable_deck=True,
+)
 def compare_model_results(
     X_val: pd.DataFrame,
     y_val: pd.DataFrame,
     models: List[RandomForestClassifier],
-    hyperparameters: List[Dict[str, Optional[float|str|int|None]]],
-    force_plot: bool = False
+    hyperparameters: List[Dict[str, Optional[float | str | int | None]]],
+    force_plot: bool = False,
 ) -> RandomForestClassifier:
     """
     Compares the results of different models on a given dataframe using specified hyperparameters
@@ -153,18 +190,18 @@ def compare_model_results(
 
     # Collect all the scores and hyperparameters
     search_parameters_dict = {}
-    search_parameters_dict['scores'] = []
+    search_parameters_dict["scores"] = []
     for name in hyperparameters[0].keys():
         search_parameters_dict[name] = []
     for model in models:
         yhat = model.predict(X_val)
-        score = f1_score(y_pred=yhat, y_true=y_val, average='macro')
-        search_parameters_dict['scores'].append(score)
+        score = f1_score(y_pred=yhat, y_true=y_val, average="macro")
+        search_parameters_dict["scores"].append(score)
         for name in hyperparameters[0].keys():
             search_parameters_dict[name].append(getattr(model, name))
 
     # Select the best model
-    best_index = np.array(search_parameters_dict['scores']).argmax()
+    best_index = np.array(search_parameters_dict["scores"]).argmax()
     best_model = models[best_index]
 
     # Create dataframe of hyperparameters and scores
@@ -172,15 +209,16 @@ def compare_model_results(
 
     # Create figure for parameters explored
     fig = px.parallel_coordinates(
-        search_parameters, color="scores",
+        search_parameters,
+        color="scores",
         dimensions=hyperparameters[0].keys(),
         color_continuous_scale=px.colors.diverging.Tealrose,
-        color_continuous_midpoint=2
+        color_continuous_midpoint=2,
     )
     fig.update_layout(
-        title_text='Parellel Coordinates',
-        xaxis = dict(title='Hyperparameter Names'),
-        yaxis = dict(title='Hyperparameter Value')
+        title_text="Parellel Coordinates",
+        xaxis=dict(title="Hyperparameter Names"),
+        yaxis=dict(title="Hyperparameter Value"),
     )
     deck_obj = Deck("Parallel Coordinates")
     deck_obj.append(plotly.io.to_html(fig))
@@ -190,6 +228,7 @@ def compare_model_results(
         fig.show()
 
     return best_model
+
 
 def plot_confusion_matrix(y_true: pd.DataFrame, y_pred: pd.DataFrame, title: str):
     """
@@ -203,7 +242,7 @@ def plot_confusion_matrix(y_true: pd.DataFrame, y_pred: pd.DataFrame, title: str
     Returns:
         (plotly.graph_objects.Figure): The confusion matrix plot.
     """
-    
+
     # Create confusion matrix
     array = confusion_matrix(y_true, y_pred)
     labels = sorted(y_true.target.unique().tolist())
@@ -211,14 +250,16 @@ def plot_confusion_matrix(y_true: pd.DataFrame, y_pred: pd.DataFrame, title: str
     # change each element of z to type string for annotations
     z_text = [[str(y) for y in x] for x in array.tolist()]
 
-    # set up figure 
-    fig = ff.create_annotated_heatmap(array, x=labels, y=labels, annotation_text=z_text, colorscale='Viridis')
+    # set up figure
+    fig = ff.create_annotated_heatmap(
+        array, x=labels, y=labels, annotation_text=z_text, colorscale="Viridis"
+    )
 
     # add title
     fig.update_layout(
         title_text=title,
-        xaxis = dict(title='Predicted Label'),
-        yaxis = dict(title='Actual Label')
+        xaxis=dict(title="Predicted Label"),
+        yaxis=dict(title="Actual Label"),
     )
 
     # # add custom xaxis title
@@ -249,10 +290,16 @@ def plot_confusion_matrix(y_true: pd.DataFrame, y_pred: pd.DataFrame, title: str
 
     # # add colorbar
     # fig['data'][0]['showscale'] = True
-    
+
     return fig
 
-@task(container_image=sklearn_image_spec, cache=cache, cache_version=cache_version, enable_deck=True)
+
+@task(
+    container_image=sklearn_image_spec,
+    cache=cache,
+    cache_version=cache_version,
+    enable_deck=True,
+)
 def analyze_model(
     X_train: pd.DataFrame,
     y_train: pd.DataFrame,
@@ -261,7 +308,7 @@ def analyze_model(
     X_test: pd.DataFrame,
     y_test: pd.DataFrame,
     model: RandomForestClassifier,
-    force_plot: bool = False
+    force_plot: bool = False,
 ) -> None:
     """
     Analyzes the performance of a RandomForestClassifier model on a given dataframe.
@@ -282,16 +329,28 @@ def analyze_model(
     yhat_train = model.predict(X_train)
     yhat_val = model.predict(X_val)
     yhat_test = model.predict(X_test)
-    
+
     # Create confusion matrix plots
-    confusion_matrix_plot_train = plot_confusion_matrix(y_true=y_train, y_pred=yhat_train, title="Training Set")
-    confusion_matrix_plot_val = plot_confusion_matrix(y_true=y_val, y_pred=yhat_val, title="Validation Set")
-    confusion_matrix_plot_test = plot_confusion_matrix(y_true=y_test, y_pred=yhat_test, title="Test Set")
+    confusion_matrix_plot_train = plot_confusion_matrix(
+        y_true=y_train, y_pred=yhat_train, title="Training Set"
+    )
+    confusion_matrix_plot_val = plot_confusion_matrix(
+        y_true=y_val, y_pred=yhat_val, title="Validation Set"
+    )
+    confusion_matrix_plot_test = plot_confusion_matrix(
+        y_true=y_test, y_pred=yhat_test, title="Test Set"
+    )
 
     # Create classification reports
-    class_report_train = pd.DataFrame(classification_report(y_train, yhat_train, output_dict=True))
-    class_report_val = pd.DataFrame(classification_report(y_val, yhat_val, output_dict=True))
-    class_report_test = pd.DataFrame(classification_report(y_test, yhat_test, output_dict=True))
+    class_report_train = pd.DataFrame(
+        classification_report(y_train, yhat_train, output_dict=True)
+    )
+    class_report_val = pd.DataFrame(
+        classification_report(y_val, yhat_val, output_dict=True)
+    )
+    class_report_test = pd.DataFrame(
+        classification_report(y_test, yhat_test, output_dict=True)
+    )
 
     # Add to the flyte deck
     deck_obj = Deck("Confusion Matrix")
@@ -311,12 +370,13 @@ def analyze_model(
         confusion_matrix_plot_test.show()
         print(class_report_test)
 
+
 @workflow
 def training_workflow(
-    grid: Dict[str, List[Optional[float|str|int|None]]] = {
-        'max_depth': [10, 50, 100],
-        'max_features': [None, 'sqrt'],
-        'n_estimators': [100, 2000]
+    grid: Dict[str, List[Optional[float | str | int | None]]] = {
+        "max_depth": [10, 50, 100],
+        "max_features": [None, "sqrt"],
+        "n_estimators": [100, 2000],
     }
 ) -> RandomForestClassifier:
     """
@@ -338,11 +398,13 @@ def training_workflow(
     hyperparameters = create_search_grid(grid=grid)
 
     # Train models in parallel
-    partial_function = partial(train_model, X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val) 
+    partial_function = partial(
+        train_model, X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val
+    )
     map_task_obj = map_task(partial_function)
     models = map_task_obj(hyperparameters=hyperparameters)
 
-    # Return the best model 
+    # Return the best model
     best_model = compare_model_results(X_val, y_val, models, hyperparameters)
 
     # Analyze the best model
@@ -353,10 +415,11 @@ def training_workflow(
         y_val=y_val,
         X_test=X_test,
         y_test=y_test,
-        model=best_model
+        model=best_model,
     )
 
     return best_model
+
 
 if __name__ == "__main__":
     training_workflow()
